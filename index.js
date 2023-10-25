50baf6367cdb2603c0f602bdf64eacb466133994
Javier García <javiergarciarodriguez@alumnos.tuniversformacion.es>
2023-10-25 08:46:18 +0200

    Funciones añadidas

---
+++ index.js
@@ -0,0 +1,27 @@
+let saldo = 1000;
+const PIN_CORRECTO ='1234';
+let intentosrestantes = 3;
+
+function mostrarsaldo(){
+
+}
+
+function depositar(){
+
+}
+
+function retirar(){
+
+}
+
+function transferir(){
+
+}
+
+function iniciarsesion(){
+
+}
+
+function menu(){
+    
+}
